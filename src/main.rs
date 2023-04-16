37f57398274c3f3dd910f0dc73de9457bccae8d1
AdeThorMiwa <bendamyth@gmail.com>
2023-04-16 17:30:14 +0100

    App ready

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,26 @@
+use secrecy::ExposeSecret;
+use sqlx::{postgres::PgPoolOptions, PgPool};
 use std::net::TcpListener;
-
-use zero2prod::run;
+use zero2prod::{
+    configuration::get_config,
+    startup::run,
+    telemetry::{get_subscriber, init_subscriber},
+};
 
 #[tokio::main]
 async fn main() -> Result<(), std::io::Error> {
-    let listener = TcpListener::bind("127.0.0.1:8000").expect("Failed to bind to port 8000");
-    run(listener)?.await
+    // Setup logging
+    let subscriber = get_subscriber("zero2prod".into(), "info".into(), std::io::stdout);
+    init_subscriber(subscriber);
+
+    // Panic if we cant read config
+    let config = get_config().expect("Failed to read configuration.");
+    let pool = PgPoolOptions::new()
+        .acquire_timeout(std::time::Duration::from_secs(2))
+        .connect_lazy(&config.database.connection_string().expose_secret())
+        .expect("Failed to connect to Postgres");
+
+    let url = format!("{}:{}", config.application.host, config.application.port);
+    let listener = TcpListener::bind(url)?;
+    run(listener, pool)?.await
 }
--- tests/health_check.rs
+++ tests/health_check.rs
@@ -1,15 +1,83 @@
-use std::net::TcpListener;
-
+use once_cell::sync::Lazy;
 use reqwest;
-use zero2prod::run;
+use secrecy::ExposeSecret;
+use sqlx::{Connection, Executor, PgConnection, PgPool};
+use std::net::TcpListener;
+use uuid::Uuid;
+use zero2prod::{
+    configuration::{get_config, DatabaseSettings},
+    startup::run,
+    telemetry::{get_subscriber, init_subscriber},
+};
+
+static TRACING: Lazy<()> = Lazy::new(|| {
+    let default_filter_level = "info".to_string();
+    let subscriber_name = "test".to_string();
+
+    if std::env::var("TEST_LOG").is_ok() {
+        let subscriber = get_subscriber(subscriber_name, default_filter_level, std::io::stdout);
+        init_subscriber(subscriber);
+    } else {
+        let subscriber = get_subscriber(subscriber_name, default_filter_level, std::io::sink);
+        init_subscriber(subscriber);
+    }
+});
+
+pub struct AppBootstrap {
+    pub address: String,
+    pub db_pool: PgPool,
+}
+
+impl AppBootstrap {
+    async fn configure_db(config: &DatabaseSettings) -> PgPool {
+        // create db
+        let mut conn =
+            PgConnection::connect(&config.connection_string_without_db().expose_secret())
+                .await
+                .expect("Failed to connect to Postgres");
+
+        conn.execute(format!(r#"CREATE DATABASE "{}";"#, config.database_name).as_str())
+            .await
+            .expect("Failed to create db");
+
+        let conn_pool = PgPool::connect(&config.connection_string().expose_secret())
+            .await
+            .expect("Failed to connect to Postgres.");
+
+        sqlx::migrate!("./migrations")
+            .run(&conn_pool)
+            .await
+            .expect("Failed to migrate the db");
+
+        conn_pool
+    }
+
+    async fn new() -> Self {
+        Lazy::force(&TRACING);
+
+        let listener = TcpListener::bind("127.0.0.1:0").expect("Failed to bind random port");
+        let port = listener.local_addr().unwrap().port();
+        let address = format!("http://127.0.0.1:{}", port);
+
+        let mut config = get_config().expect("Failed to read config");
+        config.database.database_name = Uuid::new_v4().to_string();
+        let db_pool = Self::configure_db(&config.database).await;
+
+        let server = run(listener, db_pool.clone()).expect("Failed to bind address");
+
+        let _ = tokio::spawn(server);
+
+        AppBootstrap { address, db_pool }
+    }
+}
 
 #[tokio::test]
 async fn health_check_works() {
-    let url = spawn_app();
+    let app = AppBootstrap::new().await;
     let client = reqwest::Client::new();
 
     let response = client
-        .get(&format!("{}/health_check", url))
+        .get(&format!("{}/health_check", app.address))
         .send()
         .await
         .expect("Failed to execute request.");
@@ -21,13 +89,13 @@ async fn health_check_works() {
 #[tokio::test]
 async fn subscribe_returns_a_200_for_valid_form_data() {
     // Setup
-    let app_address = spawn_app();
+    let app = AppBootstrap::new().await;
     let client = reqwest::Client::new();
 
     // Action
     let body = "name=le%20guin&email=ursula_le_guin%40gmail.com";
     let response = client
-        .post(&format!("{}/subscriptions", &app_address))
+        .post(&format!("{}/subscriptions", &app.address))
         .header("Content-Type", "application/x-www-form-urlencoded")
         .body(body)
         .send()
@@ -35,13 +103,21 @@ async fn subscribe_returns_a_200_for_valid_form_data() {
         .expect("Failed to execute request");
 
     // Assert
-    assert_eq!(200, response.status().as_u16())
+    assert_eq!(200, response.status().as_u16());
+
+    let saved = sqlx::query!("SELECT email, name FROM subscriptions")
+        .fetch_one(&app.db_pool)
+        .await
+        .expect("Failed to fetch saved subscription");
+
+    assert_eq!(saved.email, "ursula_le_guin@gmail.com");
+    assert_eq!(saved.name, "le guin")
 }
 
 #[tokio::test]
 async fn subscribe_returns_a_400_when_data_is_missing() {
     // Setup
-    let app_address = spawn_app();
+    let app = AppBootstrap::new().await;
     let client = reqwest::Client::new();
     let test_cases = vec![
         ("name=le%20guin", "missing the email"),
@@ -52,7 +128,7 @@ async fn subscribe_returns_a_400_when_data_is_missing() {
     for (invalid_body, error_message) in test_cases {
         // Action
         let response = client
-            .post(&format!("{}/subscriptions", &app_address))
+            .post(&format!("{}/subscriptions", &app.address))
             .header("Content-Type", "application/x-www-form-urlencoded")
             .body(invalid_body)
             .send()
@@ -67,14 +143,4 @@ async fn subscribe_returns_a_400_when_data_is_missing() {
             error_message
         )
     }
-}
-
-fn spawn_app() -> String {
-    let listener = TcpListener::bind("127.0.0.1:0").expect("Failed to bind random port");
-    let port = listener.local_addr().unwrap().port();
-    let server = run(listener).expect("Failed to bind address");
-
-    let _ = tokio::spawn(server);
-
-    format!("http://127.0.0.1:{}", port)
 }
