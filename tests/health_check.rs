80fed3f780ebe45f9394ef38df6e672b18b0f68b
Heroku-Deploy <undefined>
2023-04-22 11:05:27 +0100

    update test to check against invalid inputs

--- tests/health_check.rs
+++ tests/health_check.rs
@@ -143,7 +143,7 @@ async fn subscribe_returns_a_400_when_data_is_missing() {
 }
 
 #[tokio::test]
-async fn subscribe_returns_a_200_when_fields_are_present_but_empty() {
+async fn subscribe_returns_a_400_when_fields_are_present_but_invalid() {
     // Arrange
     let app = AppBootstrap::new().await;
     let client = reqwest::Client::new();
@@ -165,9 +165,9 @@ async fn subscribe_returns_a_200_when_fields_are_present_but_empty() {
 
         // Assert
         assert_eq!(
-            200,
+            400,
             response.status().as_u16(),
-            "The API did not a 200 OK when the payload was {}",
+            "The API did not return 400 Bad Request when the payload was {}",
             description
         )
     }
