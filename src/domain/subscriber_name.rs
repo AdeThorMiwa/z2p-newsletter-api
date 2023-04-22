6ea72a5db4ecc3a0b8bd5c6dbeecf55a7b0439e1
Heroku-Deploy <undefined>
2023-04-22 11:02:41 +0100

    move entity validation to domain

---
+++ src/domain/subscriber_name.rs
@@ -0,0 +1,26 @@
+use unicode_segmentation::UnicodeSegmentation;
+
+pub struct SubscriberName(String);
+
+impl SubscriberName {
+    pub fn parse(name: String) -> Self {
+        let is_empty_or_whitespace = name.trim().is_empty();
+
+        let is_too_long = name.graphemes(true).count() > 256;
+
+        let forbidden_characters = ['/', '(', ')', '"', '<', '>', '\\', '{', '}'];
+        let contains_forbidden_characters = name.chars().any(|c| forbidden_characters.contains(&c));
+
+        if is_empty_or_whitespace || is_too_long || contains_forbidden_characters {
+            panic!("{} is not a valid subscriber name.", name)
+        } else {
+            Self(name)
+        }
+    }
+}
+
+impl AsRef<str> for SubscriberName {
+    fn as_ref(&self) -> &str {
+        &self.0
+    }
+}
--- src/routes/subscriptions.rs
+++ src/routes/subscriptions.rs
@@ -1,8 +1,9 @@
 use actix_web::{web, HttpResponse};
 use chrono::Utc;
 use sqlx::PgPool;
-use unicode_segmentation::UnicodeSegmentation;
 use uuid::Uuid;
+
+use crate::domain::{new_subscriber::NewSubscriber, subscriber_name::SubscriberName};
 
 #[derive(serde::Deserialize)]
 pub struct FormData {
@@ -19,11 +20,12 @@ pub struct FormData {
     )
 )]
 pub async fn subscribe(form: web::Form<FormData>, pool: web::Data<PgPool>) -> HttpResponse {
-    if !is_valid_name(&form.name) {
-        return HttpResponse::BadRequest().finish();
-    }
+    let new_subscriber = NewSubscriber {
+        email: form.0.email,
+        name: SubscriberName::parse(form.0.name),
+    };
 
-    match insert_subscriber(&pool, &form).await {
+    match insert_subscriber(&pool, &new_subscriber).await {
         Ok(_) => HttpResponse::Ok().finish(),
         Err(_) => HttpResponse::InternalServerError().finish(),
     }
@@ -31,17 +33,20 @@ pub async fn subscribe(form: web::Form<FormData>, pool: web::Data<PgPool>) -> Ht
 
 #[tracing::instrument(
     name = "Saving new subscriber details in the database",
-    skip(form, pool)
+    skip(new_subscriber, pool)
 )]
-pub async fn insert_subscriber(pool: &PgPool, form: &FormData) -> Result<(), sqlx::Error> {
+pub async fn insert_subscriber(
+    pool: &PgPool,
+    new_subscriber: &NewSubscriber,
+) -> Result<(), sqlx::Error> {
     sqlx::query!(
         r#"
     INSERT INTO subscriptions (id, email, name, subscribed_at)
     VALUES ($1, $2, $3, $4)
     "#,
         Uuid::new_v4(),
-        form.email,
-        form.name,
+        new_subscriber.email,
+        new_subscriber.name.as_ref(),
         Utc::now()
     )
     .execute(pool)
@@ -52,15 +57,4 @@ pub async fn insert_subscriber(pool: &PgPool, form: &FormData) -> Result<(), sql
     })?;
 
     Ok(())
-}
-
-pub fn is_valid_name(name: &str) -> bool {
-    let is_empty_or_whitespace = name.trim().is_empty();
-
-    let is_too_long = name.graphemes(true).count() > 256;
-
-    let forbidden_characters = ['/', '(', ')', '"', '<', '>', '\\', '{', '}'];
-    let contains_forbidden_characters = name.chars().any(|c| forbidden_characters.contains(&c));
-
-    !(is_empty_or_whitespace || is_too_long || contains_forbidden_characters)
 }
