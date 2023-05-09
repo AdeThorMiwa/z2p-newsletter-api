1afa30a42aa1152c440bbb17a51e1cf8afaa30f1
Heroku-Deploy <undefined>
2023-05-09 21:22:16 +0100

    Prevent timing attack by making sure both failing and passing test takes roughly the same time to execute

--- tests/api/helpers.rs
+++ tests/api/helpers.rs
@@ -1,4 +1,4 @@
-use argon2::{password_hash::SaltString, Argon2, PasswordHasher};
+use argon2::{password_hash::SaltString, Algorithm, Argon2, Params, PasswordHasher, Version};
 use once_cell::sync::Lazy;
 use sqlx::{Connection, Executor, PgConnection, PgPool};
 use uuid::Uuid;
@@ -36,9 +36,9 @@ pub struct ConfirmationLinks {
 }
 
 pub struct TestUser {
-    user_id: Uuid,
-    username: String,
-    password: String,
+    pub user_id: Uuid,
+    pub username: String,
+    pub password: String,
 }
 
 impl AppBootstrap {
@@ -160,10 +160,14 @@ impl TestUser {
         let salt = SaltString::generate(&mut rand::thread_rng());
         // We don't care about the exact Argon2 parameters here
         // given that it's for testing purposes!
-        let password_hash = Argon2::default()
-            .hash_password(self.password.as_bytes(), &salt)
-            .unwrap()
-            .to_string();
+        let password_hash = Argon2::new(
+            Algorithm::Argon2id,
+            Version::V0x13,
+            Params::new(15000, 2, 1, None).unwrap(),
+        )
+        .hash_password(self.password.as_bytes(), &salt)
+        .unwrap()
+        .to_string();
 
         sqlx::query!(
             "INSERT INTO users (user_id, username, password_hash) VALUES ($1, $2, $3)",
